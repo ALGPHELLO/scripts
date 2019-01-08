c4ddbc02b56bb165ae5a1a29100dd3624a9134e4
Khaled Necir <khaledannab@gmail.com>
2019-01-08 14:08:04 +0100

    Update arch-setup.sh

--- personal-setup/arch-setup.sh
+++ personal-setup/arch-setup.sh
@@ -1,5 +1,5 @@
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
+git config --global user.name "ALGPHELLO"
+git config --global user.email "khaledannab@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
 echo "" >> ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
