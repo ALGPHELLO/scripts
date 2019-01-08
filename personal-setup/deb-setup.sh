f5d2402e0fb068f2f66144c4268a90a4bfb7c6d8
Khaled Necir <khaledannab@gmail.com>
2019-01-08 14:08:36 +0100

    Update deb-setup.sh

--- personal-setup/deb-setup.sh
+++ personal-setup/deb-setup.sh
@@ -1,6 +1,6 @@
 sudo install utils/hub /usr/local/bin/hub
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
+git config --global user.name "ALGPHELLO"
+git config --global user.email "khaledannab@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
 echo "" >> ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
